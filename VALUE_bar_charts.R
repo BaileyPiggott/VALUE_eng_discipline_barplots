43d75719c16bc00d251985049dc2683fa6c05a6b
Bailey <baileypiggott@gmail.com>
2015-07-22 10:14:01 -0400

    Add sample sizes to graph and easy change between VALUE rubrics

--- VALUE_bar_charts.R
+++ VALUE_bar_charts.R
@@ -3,23 +3,40 @@
 
 source("set_up.R")
 
+# Chooses:
+value_scores <- ps
+graph_title <- "Average Problem Solving VALUE Scores"
+palette_colour <- "Greens"
+
+value_scores <- ct
+graph_title <- "Average Critical Thinking VALUE Scores"
+palette_colour <- "Blues"
+
+value_scores <- wc
+graph_title <- "Average Written Communication VALUE Scores"
+palette_colour <- "Oranges"
+
 
 ## plot
 ggplot(
-  data = ct, 
+  data = value_scores, 
   aes(
-    x = Discipline, y = score
+    x = Discipline, y = score   #, fill = Discipline
     )
   )+
   geom_bar(
     stat = "identity",
-    position = "dodge"       
+    position = "dodge"
     ) + 
-  coord_cartesian(ylim = c(0, 4)) + 
+  coord_cartesian(ylim = c(0, 4)) +  
   theme(
     axis.line = element_line("grey"), #change colour of x and y axis
     panel.grid.major.y = element_line("grey"), #change horizonatal line colour (from white)
-    panel.background = element_rect("white") #change background colour
+    panel.background = element_rect("white"), #change background colour
+    panel.grid.major.x = element_blank()
     ) +
-  labs(title = "VALUE Data", x = "Discipline", y = "Average Rubric Level") 
+  labs(title = graph_title,  x = "Discipline", y = "Average Rubric Level")
+
+#+
+ # scale_fill_brewer(palette = palette_colour)
 
--- set_up.R
+++ set_up.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(magrittr)
+library(RColorBrewer)
 # load libraries
 
 #load data
@@ -24,52 +25,55 @@ df <- bind_rows(bailey_scores, alyssa_scores) # dataframe now has two rows per d
 #subset by discipline
 all_eng <- df %>% subset(Discipline != "CHEE")  # take out chem scores
 n_eng <- nrow(all_eng)/2
-all_eng <- all_eng %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "ENG") # average of scores per criteria and change discipline to eng for plotting
+all_eng <- all_eng %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("Total\nn = ", n_eng)) # average of scores per criteria and change discipline to eng for plotting
 
 mech <- df %>% subset(Discipline == "MECH")
 n_mech <- nrow(mech)/2 #divide by two because the two rater scores are separate rows
-mech <- mech %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "MECH")
+mech <- mech %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("MECH\nn = ", n_mech))
 
 ece <- df %>% subset(Discipline == "ELEC") #elec and cmpe
 n_ece <- nrow(ece)/2 #divide by two because the two rater scores are separate rows
-ece <- ece %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "ECE")
+ece <- ece %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("ECE\nn = ", n_ece))
 
 civl <- df %>% subset(Discipline == "CIVL")
 n_civl <- nrow(civl)/2 #divide by two because the two rater scores are separate rows
-civl <- civl %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "CIVL")
+civl <- civl %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("CIVL\nn = ", n_civl))
 
 chee <- df %>% subset(Discipline == "CHEE") #chee and ench
 n_chee <- nrow(chee)/2 #divide by two because the two rater scores are separate rows
-chee <- chee %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "CHEE")
+chee <- chee %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("CHEE\nn = ", n_chee))
 
 mine <- df %>% subset(Discipline == "MINE")
 n_mine <- nrow(mine)/2 #divide by two because the two rater scores are separate rows
-mine <- mine %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "MINE")
+mine <- mine %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("MINE\nn = ", n_mine))
 
 geoe <- df %>% subset(Discipline == "GEOE")
 n_geoe <- nrow(geoe)/2 #divide by two because the two rater scores are separate rows
-geoe <- geoe %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "GEOE")
+geoe <- geoe %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("GEOE\nn = ", n_geoe))
 
 enph <- df %>% subset(Discipline == "ENPH")
 n_enph <- nrow(enph)/2 #divide by two because the two rater scores are separate rows
-enph <- enph %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "ENPH")
+enph <- enph %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("ENPH\nn = ", n_enph))
 
 mthe <- df %>% subset(Discipline == "MTHE")
 n_mthe <- nrow(mthe)/2 #divide by two because the two rater scores are separate rows
-mthe <- mthe %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = "MTHE")
+mthe <- mthe %>% summarise_each(funs(mean(., na.rm = TRUE))) %>% mutate(Discipline = paste0("MTHE\nn = ", n_mthe))
 
-all_means <- bind_rows(mech, ece, civl, chee, mine, geoe, enph, mthe) %>% select(-Group, -Section)
+all_means <- bind_rows(mech, ece, civl, chee, mine, geoe, enph, mthe, all_eng) %>% select(-Group, -Section)
 
 ps <- all_means %>% 
   select(Discipline, PS1.1, PS2.1, PS3.1, PS4.1, PS5.1,PS6.1) %>% 
-  transmute(Discipline, score = rowMeans( .[, -1] , na.rm = TRUE)) # average all rows, except the first column(discipline)
+  transmute(Discipline, score = rowMeans( .[, -1] , na.rm = TRUE)) %>% # average all rows, except the first column(discipline)
+  arrange(score)
 
 ct <- all_means %>% 
   select(Discipline, CT1.1, CT2.1, CT3.1, CT4.1, CT5.1) %>% 
-  transmute(Discipline, score = rowMeans( .[, -1] , na.rm = TRUE))
+  transmute(Discipline, score = rowMeans( .[, -1] , na.rm = TRUE)) %>% #average rows
+  arrange(score)
 
 wc <- all_means %>% 
   select(Discipline, WC1.1, WC2.1, WC3.1, WC4.1, WC5.1) %>% 
-  transmute(Discipline, score = rowMeans( .[, -1] , na.rm = TRUE))
+  transmute(Discipline, score = rowMeans( .[, -1] , na.rm = TRUE)) %>% #average rows
+  arrange(score)
 
 
