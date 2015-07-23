f503053b37dfd2b8c628f89dc52c5ecf8906da97
Bailey <baileypiggott@gmail.com>
2015-07-23 13:15:06 -0400

    change x axis text to VALUE criteria

--- VALUE_allcriteria_bar.R
+++ VALUE_allcriteria_bar.R
@@ -2,7 +2,8 @@
 # bar chart with all VALUE criteria
 
 eng_totals <- all_eng[c("Discipline", "PS1.1", "PS2.1", "PS3.1", "PS4.1", "PS5.1", "PS6.1", "CT1.1", "CT2.1", "CT3.1", "CT4.1", "CT5.1", "WC1.1", "WC2.1", "WC3.1", "WC4.1", "WC5.1")]
-value <- c("ps","ps","ps","ps","ps","ps","ct","ct","ct","ct","ct","wc","wc","wc","wc","wc")
+value <- c("a","a","a","a","a","a","b","b","b","b","b","c","c","c","c","c")
+criteria <- c("Define\nProblem", "Identify\nStrategies", "Propose\nSolutions", "Evaluate\nPotential\nSolutions", "Implement\nSolutions", "Evaluate\nOutcomes", "Explanation\nof Issues", "Evidence", "Influence of\nContext and\nAssumptions", "Student's\nPosition", "Conclusions", "Context and\nPurpose for\nWriting", "Content\nDevelopment", "Genre and\nDisciplinary\nConventions", "Sources and\nEvidence", "Syntax and\nMechanics")
 
 
 eng_totals <- eng_totals %>% 
@@ -10,11 +11,15 @@ eng_totals <- eng_totals %>%
   gather(learning_outcome, mean, PS1.1:WC5.1) %>%
   cbind(value) # add column defining type of VALUE criteria (ps or ct or wc)
 
+
+
+##plot -------------------------------------------------
 ggplot(
   data = eng_totals, 
   aes(x = learning_outcome, y = mean, fill = value) # x value is the factor column so bars plot in ascending order
   )+
   geom_bar(stat = "identity", width = 0.5) + 
+  scale_x_discrete(labels = criteria) +
   coord_cartesian(ylim = c(0, 4)) +  
   theme(
     axis.line = element_line("grey"), #change colour of x and y axis
@@ -24,7 +29,17 @@ ggplot(
     legend.position="none", #remove legend
     plot.title = element_text(size = 15),
     axis.title = element_text(size = 14),
-    axis.text = element_text(size = 12) #size of x axis labels
-  ) +
-  labs(title = graph_title,  x = "Discipline", y = "Average Rubric Level")
+    axis.text = element_text(size = 12, angle = 90) #size of x axis labels
+    ) +
+  labs(title = graph_title,  x = "VALUE Criteria", y = "Average Rubric Level") +
+  scale_fill_manual(values = c("#33CC44", "#3388EE", "#FF8833")) +
+  annotate( # add labels for CLA mastery levels
+    "text", 
+    fontface = "bold", 
+    size = 5,
+    x = c(3.5, 9, 14), y = 3.5, 
+    label = c("Problem\nSolving", "Critical\nThinking", "Written\nCommunication"), 
+    colour = c("#33CC44", "#3388EE", "#FF8833")
+  ) 
+
 
